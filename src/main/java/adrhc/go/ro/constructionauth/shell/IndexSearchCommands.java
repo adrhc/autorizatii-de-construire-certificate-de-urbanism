a670919362a2077004b7268994d841c75efd6f11
Adrian <adrhc@outlook.com>
2024-03-22 23:52:35 +0200

    IndexSearchCommands.searchLastUpdate

--- src/main/java/adrhc/go/ro/constructionauth/shell/IndexSearchCommands.java
+++ src/main/java/adrhc/go/ro/constructionauth/shell/IndexSearchCommands.java
@@ -1,5 +1,6 @@
 package adrhc.go.ro.constructionauth.shell;
 
+import adrhc.go.ro.constructionauth.datasource.index.UrlContentIndexManager;
 import adrhc.go.ro.constructionauth.datasource.index.UrlContentIndexSearcher;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,10 +15,17 @@ import static adrhc.go.ro.constructionauth.util.DebugUtils.showMatches;
 @RequiredArgsConstructor
 @Slf4j
 public class IndexSearchCommands {
+    private final UrlContentIndexManager indexManager;
     private final UrlContentIndexSearcher searcher;
 
     @ShellMethod(value = "Search the index.")
     public void search(String words) throws IOException {
+        showMatches(words, searcher.search(words));
+    }
+
+    @ShellMethod(value = "Update then search the index.")
+    public void searchLastUpdate(String words) throws IOException {
+        indexManager.updateIndex();
         showMatches(words, searcher.search(words));
     }
 }
