1aee21b898f3c1e46abcdebba004bfca7f33806a
Adrian <adrhc@outlook.com>
2024-03-22 13:51:58 +0200

    @EnableFeignClients

--- src/main/java/adrhc/go/ro/constructionauth/ConstructionAuthorizationApp.java
+++ src/main/java/adrhc/go/ro/constructionauth/ConstructionAuthorizationApp.java
@@ -1,10 +1,12 @@
 package adrhc.go.ro.constructionauth;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * Hello world!
  */
+@EnableFeignClients
 @SpringBootApplication
 public class ConstructionAuthorizationApp {
     public static void main(String[] args) {
