92777a27ec20476589e6fef271f201a9dce5ad0c
Adrian <adrhc@outlook.com>
2024-03-22 13:39:08 +0200

    CloseableIndexRepository.java

--- src/main/java/adrhc/go/ro/constructionauth/datasource/index/CloseableIndexRepository.java
+++ src/main/java/adrhc/go/ro/constructionauth/datasource/index/CloseableIndexRepository.java
@@ -3,11 +3,9 @@ package adrhc.go.ro.constructionauth.datasource.index;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
 import ro.go.adrhc.persistence.lucene.typedindex.IndexRepository;
 import ro.go.adrhc.persistence.lucene.typedindex.TypedIndexContext;
 
-@Component
 @Getter
 @RequiredArgsConstructor
 @Slf4j
