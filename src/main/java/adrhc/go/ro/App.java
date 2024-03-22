18b516915be26b0163714ae724716bb6009dffb6
Adrian <adrhc@outlook.com>
2024-03-22 11:14:42 +0200

    init

---
+++ src/main/java/adrhc/go/ro/App.java
@@ -0,0 +1,13 @@
+package adrhc.go.ro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
