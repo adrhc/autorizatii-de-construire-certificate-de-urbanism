d0741d27d47f2b89202b1cf04f855ee2bf77991c
Adrian <adrhc@outlook.com>
2024-03-28 16:41:08 +0200

    CloseableIndexRepository.java

--- src/main/java/ro/go/adrhc/constructionauth/datasource/index/CloseableIndexRepository.java
+++ src/main/java/ro/go/adrhc/constructionauth/datasource/index/CloseableIndexRepository.java
@@ -1,15 +1,29 @@
 package ro.go.adrhc.constructionauth.datasource.index;
 
+import jakarta.annotation.PreDestroy;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import ro.go.adrhc.persistence.lucene.typedindex.IndexRepository;
 import ro.go.adrhc.persistence.lucene.typedindex.TypedIndexContext;
 
+import java.io.Closeable;
+import java.io.IOException;
+
 @Getter
 @RequiredArgsConstructor
 @Slf4j
-public class CloseableIndexRepository {
+public class CloseableIndexRepository implements Closeable {
     private final TypedIndexContext<UrlContentIndexRecord> typedIndexContext;
     private final IndexRepository<String, UrlContentIndexRecord> indexRepository;
+
+    @PreDestroy
+    protected void preDestroy() throws IOException {
+        close();
+    }
+
+    @Override
+    public void close() throws IOException {
+        typedIndexContext.close();
+    }
 }
