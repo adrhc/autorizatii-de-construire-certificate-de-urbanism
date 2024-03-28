e21b22a95d428f6a06135c21cdde6e70005c993d
Adrian <adrhc@outlook.com>
2024-03-28 14:12:18 +0200

    ConstructionAuthConfig.java

--- src/main/java/ro/go/adrhc/module/ConstructionAuthConfig.java
+++ src/main/java/ro/go/adrhc/module/ConstructionAuthConfig.java
@@ -3,13 +3,19 @@ package ro.go.adrhc.module;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.shell.standard.ShellComponent;
+import ro.go.adrhc.constructionauth.config.AppConfiguration;
 
 @Configuration
 @ComponentScan(
         basePackageClasses = SpringBootApplication.class,
-        excludeFilters = @ComponentScan.Filter(classes = {
-                SpringBootApplication.class, ShellComponent.class
-        }))
+        excludeFilters = {
+                @ComponentScan.Filter(classes = {
+                        SpringBootApplication.class, ShellComponent.class
+                }),
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,
+                        classes = AppConfiguration.class)
+        })
 public class ConstructionAuthConfig {
 }
